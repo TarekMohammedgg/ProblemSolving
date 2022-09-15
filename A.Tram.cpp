b354cd7de3cb2f70ec62bb9dd86217cb8988cae7
tarek <tarekmohammad022@gmail.com>
2022-09-15 14:17:12 +0200

    Tram

---
+++ A.Tram.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<ctype.h>
+#include<set>
+using namespace std ;
+int main() {
+int number ;
+cin>> number ;
+cout << "Your Number Is : " << number << endl ;
+cout << "Thank You!"<< endl ;
+
+return 0; }
